a5888f860eee137983cd985dfb62572ee373a719
Oscar Levin <oscar.levin@gmail.com>
2025-12-05 02:38:11 +0000

    Update pelican settings

---
+++ site/pelican_settings.py
@@ -0,0 +1,7 @@
+# This is a temporary version of the settings that would normally go in "site.json"
+# Since we are using pelican directly instead of through the CLI, it is easier to have these here.
+
+THEME = "./site/theme"
+SITENAME = "PreTeXt"
+SITESUBTITLE = "Write Once, Read Anywhere"
+STATIC_PATHS = ["examples", "doc"]
